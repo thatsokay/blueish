0aabb73f109064737e5a44761f3db179d9e1786c
Andre Wong <andre@andre-wong.com>
2018-11-13 22:03:36 +1000

    Added bulma and sass

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import './App.css'
+import '../../sass/main.sass'
 import HueSquare from '../../components/HueSquare'
 
 class App extends Component {
@@ -11,12 +12,22 @@ class App extends Component {
     }
   }
 
+  handleStartClick(event) {
+    event.preventDefault()
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <HueSquare hue={this.state.hue} />
           <h1>Blueish</h1>
+          <button
+            className="button is-light is-outlined is-large"
+            onClick={this.handleStartClick}
+          >
+            Start
+          </button>
         </header>
       </div>
     )
