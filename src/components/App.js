2ad70ff032b98a198930362c76a87bfbb0adb569
Andre Wong <andre@andre-wong.com>
2019-09-10 21:22:51 +1000

    Use hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,163 +1,125 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
 import HueSquare from './HueSquare'
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      displayHue: null,
-      lowerHue: 250,
-      upperHue: 270,
-      numHues: 5,       // Number of hues to show including both bounds
-      hues: [],
-      pool: [],         // Hues that haven't been displayed yet
-      choices: {},
-    }
+const lowerHue = 250
+const upperHue = 270
+const numHues = 5 // Number of hues to show including both bounds
 
-    this.handleStart = this.handleStart.bind(this)
-    this.handleChoiceCreator = this.handleChoiceCreator.bind(this)
-    this.handleStartAgain = this.handleStartAgain.bind(this)
-    this.nextHue = this.nextHue.bind(this)
-  }
+const step = (upperHue - lowerHue) / (numHues - 1)
+const hues = [...Array(numHues).keys()].map(
+  i => lowerHue + step * i
+)
 
-  handleStart(event) {
-    event.preventDefault()
-    var step = (this.state.upperHue - this.state.lowerHue) / (this.state.numHues - 1)
-    var hues = []
-    for (let i = this.state.lowerHue; i <= this.state.upperHue; i += step) {
-      hues.push(i)
-    }
-    this.setState({
-      hues,
-      pool: hues,
-    })
-    setTimeout(() => this.nextHue(), 0)
-  }
+const App = () => {
+  const [displayHue, setDisplayHue] = useState(null)
+  const [huePool, setHuePool] = useState(hues)
+  const [choices, setChoices] = useState({})
 
-  handleChoiceCreator = choice => event => {
-    event.preventDefault()
-    this.setState(state => ({
-      choices: Object.assign({}, state.choices, {
-        [state.displayHue]: choice,
-      })
-    }))
-    setTimeout(() => this.nextHue(), 0)
-  }
-
-  handleStartAgain(event) {
-    event.preventDefault()
-    this.setState({choices: {}})
-  }
-
-  nextHue() {
+  const nextHue = () => {
     // Choose a hue that hasn't been displayed yet and display it
-    if (this.state.pool.length) {
-      var selected = Math.floor(Math.random() * this.state.pool.length)
-      this.setState(state => ({
-        displayHue: state.pool[selected],
-        pool: state.pool.filter((item, index) => index !== selected),
-      }))
+    if (huePool.length) {
+      const selectedIndex = Math.floor(Math.random() * huePool.length)
+      setDisplayHue(huePool[selectedIndex])
+      setHuePool(huePool.filter((_, i) => i !== selectedIndex))
     } else {
-      this.setState({displayHue: null})
+      setDisplayHue(null)
+      setHuePool(hues)
     }
   }
 
-  renderLanding() {
-    return (
-      <div className="container">
-        <HueSquare hue={this.state.displayHue} />
-        <h1>Blueish</h1>
+  const handleChoice = choice => () => {
+    setChoices({ ...choices, [displayHue]: choice })
+    nextHue()
+  }
+
+  const handleRestart = () => {
+    setChoices({})
+  }
+
+  const Home = () => (
+    <div className="container">
+      <HueSquare hue={displayHue} />
+      <h1>Blueish</h1>
+      <button
+        className="button is-light is-outlined is-large"
+        onClick={nextHue}
+      >
+        Start
+      </button>
+    </div>
+  )
+
+  const Question = () => (
+    <div className="container">
+      <HueSquare hue={displayHue} />
+      <h1>Is the box blue or purple?</h1>
+      <div className="buttons has-addons">
         <button
-          className="button is-light is-outlined is-large"
-          onClick={this.handleStart}
+          className="button is-light is-outlined is-large choice"
+          onClick={handleChoice('Blue')}
         >
-          Start
+          Blue
+        </button>
+        <button
+          className="button is-light is-outlined is-large choice"
+          onClick={handleChoice('Purple')}
+        >
+          Purple
         </button>
       </div>
-    )
-  }
+    </div>
+  )
 
-  renderQuestion() {
-    return (
-      <div className="container">
-        <HueSquare hue={this.state.displayHue} />
-        <h1>Is the box blue or purple?</h1>
-        <div className="buttons has-addons">
-          <button
-            className="button is-light is-outlined is-large choice"
-            onClick={this.handleChoiceCreator('Blue')}
-          >
-            Blue
-          </button>
-          <button
-            className="button is-light is-outlined is-large choice"
-            onClick={this.handleChoiceCreator('Purple')}
-          >
-            Purple
-          </button>
-        </div>
+  const Results = () => (
+    <div className="container">
+      <h1>Your answers</h1>
+      <div className="columns">
+        {hues.map(hue => (
+          <div className="column" key={hue}>
+            <HueSquare hue={hue} size={20} />
+            <p>{choices[hue]}</p>
+          </div>
+        ))}
       </div>
-    )
-  }
-
-  renderResults() {
-    return (
-      <div className="container">
-        <h1>Your answers</h1>
-        <div className="columns">
-          {this.state.hues.map(hue => (
-            <div className="column" key={hue}>
-              <HueSquare hue={hue} size={20} />
-              <p>{this.state.choices[hue]}</p>
-            </div>
-          ))}
-        </div>
-        <button
-          className="button is-light is-outlined is-large"
-          onClick={this.handleStartAgain}
+      <button
+        className="button is-light is-outlined is-large"
+        onClick={handleRestart}
+      >
+        Start Again
+      </button>
+      <p className="text">
+        Have you ever disagreed with someone when describing the colour of an
+        object?
+        <br />
+        People assign
+        {' '}
+        <a
+          className="App-link"
+          href="https://en.wikipedia.org/wiki/Color_term"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          Start Again
-        </button>
-        <p className="text">
-          Have you ever disagreed with someone when describing the colour of an
-          object?
-          <br />
-          People assign
-          {' '}
-          <a
-            className="App-link"
-            href="https://en.wikipedia.org/wiki/Color_term"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            colour terms
-          </a>
-          {' '}
-          based on their language, culture, and individual perception.
-          <br />
-          Where do you draw the line between these terms?
-        </p>
-      </div>
-    )
-  }
+          colour terms
+        </a>
+        {' '}
+        based on their language, culture, and individual perception.
+        <br />
+        Where do you draw the line between these terms?
+      </p>
+    </div>
+  )
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header container">
-          {(this.state.displayHue === null && (
-            (Object.keys(this.state.choices).length &&
-              this.renderResults()
-            ) ||
-            this.renderLanding()
-          )) ||
-            this.renderQuestion()
-          }
-        </header>
-      </div>
-    )
-  }
+  return (
+    <div className="App">
+      <header className="App-header container">
+        {displayHue === null
+          ? (Object.keys(choices).length ? <Results /> : <Home />)
+          : <Question />
+        }
+      </header>
+    </div>
+  )
 }
 
 export default App
